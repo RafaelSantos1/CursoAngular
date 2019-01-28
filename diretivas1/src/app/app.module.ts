cfb8a0c2c4522feeb4d21f529e75a15436a476c0
Rafael dos Santos de Oliveira Lima <RafaelSOL@tce.rj.gov.br>
2019-01-28 13:05:38 -0200

    ngFor

--- diretivas1/src/app/app.module.ts
+++ diretivas1/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiretivaNgswitchComponent
+    DiretivaNgswitchComponent,
+    DiretivaNgforComponent
   ],
   imports: [
     BrowserModule,
