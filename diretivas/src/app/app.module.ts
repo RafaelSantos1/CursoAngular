c3bd53d162f633bc076e243872dbe724de057436
Rafael dos Santos de Oliveira Lima <RafaelSOL@tce.rj.gov.br>
2019-01-28 11:18:25 -0200

    diretiva ngSwitch

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiretivaNgswitchComponent
   ],
   imports: [
     BrowserModule,
