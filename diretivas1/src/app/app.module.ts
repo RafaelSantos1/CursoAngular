970955ea0ee885d6ebf0ad192b5470cbdd4e498f
Rafael dos Santos de Oliveira Lima <RafaelSOL@tce.rj.gov.br>
2019-01-28 11:39:18 -0200

    nova diretiva

---
+++ diretivas1/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DiretivaNgswitchComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
