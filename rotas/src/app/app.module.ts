13b71ef3b7989da4441f59eba84e57969ca7d045
Rafael dos Santos de Oliveira Lima <RafaelSOL@tce.rj.gov.br>
2019-02-01 15:22:58 -0200

    Rotas: Definindo e extraindo parâmetros de roteamento

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { routing } from './app.routing';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CursosComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    CursoDetalheComponent
   ],
   imports: [
     BrowserModule,
--- rotas/src/app/app.routing.ts
+++ rotas/src/app/app.routing.ts
@@ -1,3 +1,4 @@
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { Routes, RouterModule } from '@angular/router';
@@ -7,7 +8,8 @@ import { ModuleWithProviders } from '@angular/compiler/src/core';
 const APP_ROUTES: Routes = [
     {path: '', component: HomeComponent},
     {path: 'login', component: LoginComponent },
-    {path: 'cursos', component: CursosComponent }
+    {path: 'cursos', component: CursosComponent },
+    {path: 'curso/:id', component: CursoDetalheComponent }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
