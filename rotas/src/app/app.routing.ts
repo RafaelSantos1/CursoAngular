69aa3501274fcf9825c66d4ca12ff20c8717225b
Rafael dos Santos de Oliveira Lima <RafaelSOL@tce.rj.gov.br>
2019-02-01 14:04:40 -0200

    rotas

---
+++ rotas/src/app/app.routing.ts
@@ -0,0 +1,14 @@
+import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ModuleWithProviders } from '@angular/compiler/src/core';
+
+const APP_ROUTES: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'login', component: LoginComponent },
+    {path: 'cursos', component: CursosComponent }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
+
